856e26c669e0e99c821735cec2faeaa0ec821bcb
Collin <96417832+Kolumnist@users.noreply.github.com>
2024-06-21 19:56:15 +0200

    finishes following particles and big thing
    Should have pushed earlier damn me...

---
+++ VFX_Morphing/Assets/Scripts/Interactions/RestartVFX.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class RestartVFX : MonoBehaviour
+{
+	private VisualEffect vfx;
+	private InputReader inputReader;
+
+	void Start()
+	{
+		vfx = GetComponent<VisualEffect>();
+	}
+
+	private void OnTriggerEnter(Collider player)
+	{
+		if (!player.CompareTag("Player")) return;
+		var playerControllerScript = player.gameObject.GetComponent<PlayerController>();
+		playerControllerScript.pressToInteract.SetActive(true);
+		inputReader = playerControllerScript.GetInputReader();
+		inputReader.Interaction = Restart;
+	}
+
+	private void OnTriggerExit(Collider player)
+	{
+		if (!player.CompareTag("Player")) return;
+		var playerControllerScript = player.gameObject.GetComponent<PlayerController>();
+		playerControllerScript.pressToInteract.SetActive(false);
+		inputReader = playerControllerScript.GetInputReader();
+		inputReader.Interaction = null;
+	}
+
+	protected void Restart()
+	{
+		vfx.Reinit();
+	}
+}
---
+++ VFX_Morphing/Assets/SetSuccTarget.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SetSuccTarget : MonoBehaviour
+{
+	private VisualEffect vfx;
+	private PlayerController playerControllerScript;
+	
+	void Start()
+	{
+		vfx = GetComponent<VisualEffect>();
+	}
+
+	private void Update()
+	{
+        if (playerControllerScript != null)
+        {
+			vfx.SetVector3("SuccTarget", playerControllerScript.gameObject.transform.position - transform.position + new Vector3(0,1.45f,0));
+		}
+    }
+
+	private void OnTriggerEnter(Collider player)
+	{
+		if (!player.CompareTag("Player")) return;
+		playerControllerScript = player.gameObject.GetComponent<PlayerController>();
+	}
+
+	private void OnTriggerExit(Collider player)
+	{
+		if (!player.CompareTag("Player")) return;
+		playerControllerScript = null;
+		vfx.SetVector3("SuccTarget", Vector3.zero);
+	}
+}
